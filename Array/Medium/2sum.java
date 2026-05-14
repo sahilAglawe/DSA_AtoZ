5421cf175322a5bd70271620dc3a6145564c4ecb
Sahil Aglawe <166900363+sahilAglawe@users.noreply.github.com>
2026-05-15 02:21:44 +0530

    Optimize twoSum method with two-pointer approach
    Refactored twoSum method to use two-pointer technique instead of hash table.

--- Array/Medium/2sum.java
+++ Array/Medium/2sum.java
@@ -1,32 +1,20 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numMap = new HashMap<>();
-        int n = nums.length;
+    public int[] twoSum(int[] numbers, int target) {
+       
+        int i = 0;
+        int j = numbers.length-1;
+        
+        while(i < j){
+        int sum = numbers[i] + numbers[j];
 
-        // Build the hash table
-        for (int i = 0; i < n; i++) {
-            numMap.put(nums[i], i);
+        if(sum == target)
+         return new int[]{i+1,j+1};
+
+         if(sum < target)
+         i++;
+         else
+         j--;
         }
-
-        // Find the complement
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
-                return new int[]{i, numMap.get(complement)};
-            }
-        }
-
-        return new int[]{}; // No solution found
+        return new int[]{-1,-1};
     }
 }
-
-// T/C : O(N) 
-
-// this solution uses a hash table to store the indices of the numbers in the array.
-
-// ex: nums = [2,7,11,15], target = 9
-// the hash table will look like this: 
-// {2:0, 7:1, 11:2, 15:3} we save the index and the number in pair .
-// for target = 9, we will check if the complement (9-2=7) exists in the table.
-// if it does , we return the indices of the two numbers.
-// in this case, we will return [0,1] as the indices of 2 and 7 respectively.
